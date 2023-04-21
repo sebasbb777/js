23cd94915e5c2bce4b9354c2573683db29441f54
yiis <giselay.09@gmail.com>
2023-04-21 09:48:24 -0600

    hola

---
+++ 01Holamundo/main.js
@@ -0,0 +1,12 @@
+const hola=()=>{
+    alert("hola mundo");
+    let nombre=prompt("cual es tu nombre?");
+    alert("hola"+nombre+"_ _");
+    
+  }
+  const suma=()=>{
+    let numero1=parseInt(prompt("Numero1:")) 
+    let numero2=parseInt(prompt("Numero2:"))
+    let suma= numero1+numero2;
+      alert("suma:"+suma);
+  }
---
+++ 04Cambiarimagen/main.js
@@ -0,0 +1,15 @@
+const uno=()=>{
+    let img=document.querySelector("#imagen");
+    img.src="imagen1.pgn.jfif";
+    
+  }
+  const dos=()=>{
+    let img=document.querySelector("#imagen");
+    img.src="imagen3.pgn.jfif";
+    
+  }
+  const tres=()=>{
+    let img=document.querySelector("#imagen");
+    img.src="img.pgn.jfif";
+    
+  }
